310ce25c3aff1b44ffd4d614b5a6d8127b4837f9
WenjieDu <wenjay.du@gmail.com>
2022-05-21 17:04:59 +0800

    refactor: make config files more friendly to users;

--- dataset_generating_scripts/dataset_generating.sh
+++ dataset_generating_scripts/dataset_generating.sh
@@ -2,7 +2,7 @@
 python gene_PhysioNet2012_dataset.py \
   --raw_data_path RawData/Physio2012_mega/mega \
   --outcome_files_dir RawData/Physio2012_mega/ \
-  --dataset_name physio2012_37feats \
+  --dataset_name physio2012_37feats_01masked \
   --saving_path ../generated_datasets
 
 # generate UCI Beijing air quality dataset
@@ -10,7 +10,7 @@ python gene_UCI_BeijingAirQuality_dataset.py \
   --file_path RawData/AirQuality/PRSA_Data_20130301-20170228 \
   --seq_len 24 \
   --artificial_missing_rate 0.1 \
-  --dataset_name UCIair_seqlen24_01masked \
+  --dataset_name AirQuality_seqlen24_01masked \
   --saving_path ../generated_datasets
 
 # generate UCI electricity dataset
@@ -18,6 +18,6 @@ python gene_UCI_electricity_dataset.py \
   --file_path RawData/Electricity/LD2011_2014.txt \
   --artificial_missing_rate 0.1 \
   --seq_len 100 \
-  --dataset_name electricity_seqlen100_01masked \
+  --dataset_name Electricity_seqlen100_01masked \
   --saving_path ../generated_datasets
 
