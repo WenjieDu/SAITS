ba8ce6c5cef0d1ab86b21a9274f10f165c12daf2
Wenjie Du <wenjay.du@gmail.com>
2023-04-24 19:24:25 +0800

    refactor: refactoring all code;

--- Simple_RNN_on_imputed_data.py
+++ Simple_RNN_on_imputed_data.py
@@ -1,6 +1,29 @@
 """
 The simple RNN classification model for imputed dataset PhysioNet-2012.
+
+If you use code in this repository, please cite our paper as below. Many thanks.
+
+@article{DU2023SAITS,
+title = {{SAITS: Self-Attention-based Imputation for Time Series}},
+journal = {Expert Systems with Applications},
+volume = {219},
+pages = {119619},
+year = {2023},
+issn = {0957-4174},
+doi = {https://doi.org/10.1016/j.eswa.2023.119619},
+url = {https://www.sciencedirect.com/science/article/pii/S0957417423001203},
+author = {Wenjie Du and David Cote and Yan Liu},
+}
+
+or
+
+Wenjie Du, David Cote, and Yan Liu. SAITS: Self-Attention-based Imputation for Time Series. Expert Systems with Applications, 219:119619, 2023. https://doi.org/10.1016/j.eswa.2023.119619
+
 """
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: GLP-v3
+
 import argparse
 import os
 from datetime import datetime
@@ -29,13 +52,21 @@ class LoadImputedDataAndLabel(Dataset):
 
     def __getitem__(self, idx):
         return (
-            torch.from_numpy(self.imputed_data[idx].astype('float32')),
-            torch.from_numpy(self.labels[idx].astype('float32')),
+            torch.from_numpy(self.imputed_data[idx].astype("float32")),
+            torch.from_numpy(self.labels[idx].astype("float32")),
         )
 
 
 class ImputedDataLoader:
-    def __init__(self, original_data_path, imputed_data_path, seq_len, feature_num, batch_size=128, num_workers=4):
+    def __init__(
+        self,
+        original_data_path,
+        imputed_data_path,
+        seq_len,
+        feature_num,
+        batch_size=128,
+        num_workers=4,
+    ):
         """
         original_data_path: path of original dataset, which contains classification labels
         imputed_data_path: path of imputed data
@@ -45,23 +76,27 @@ class ImputedDataLoader:
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-        with h5py.File(imputed_data_path, 'r') as hf:
-            imputed_train_set = hf['imputed_train_set'][:]
-            imputed_val_set = hf['imputed_val_set'][:]
-            imputed_test_set = hf['imputed_test_set'][:]
+        with h5py.File(imputed_data_path, "r") as hf:
+            imputed_train_set = hf["imputed_train_set"][:]
+            imputed_val_set = hf["imputed_val_set"][:]
+            imputed_test_set = hf["imputed_test_set"][:]
 
-        with h5py.File(original_data_path, 'r') as hf:
-            train_set_labels = hf['train']['labels'][:]
-            val_set_labels = hf['val']['labels'][:]
-            test_set_labels = hf['test']['labels'][:]
+        with h5py.File(original_data_path, "r") as hf:
+            train_set_labels = hf["train"]["labels"][:]
+            val_set_labels = hf["val"]["labels"][:]
+            test_set_labels = hf["test"]["labels"][:]
 
         self.train_set = LoadImputedDataAndLabel(imputed_train_set, train_set_labels)
         self.val_set = LoadImputedDataAndLabel(imputed_val_set, val_set_labels)
         self.test_set = LoadImputedDataAndLabel(imputed_test_set, test_set_labels)
 
     def get_loaders(self):
-        train_loader = DataLoader(self.train_set, self.batch_size, shuffle=True, num_workers=self.num_workers)
-        val_loader = DataLoader(self.val_set, self.batch_size, shuffle=True, num_workers=self.num_workers)
+        train_loader = DataLoader(
+            self.train_set, self.batch_size, shuffle=True, num_workers=self.num_workers
+        )
+        val_loader = DataLoader(
+            self.val_set, self.batch_size, shuffle=True, num_workers=self.num_workers
+        )
         test_loader = DataLoader(self.test_set, self.batch_size, shuffle=False)
         return train_loader, val_loader, test_loader
 
@@ -69,7 +104,9 @@ class ImputedDataLoader:
 class SimpleRNNClassification(torch.nn.Module):
     def __init__(self, feature_num, rnn_hidden_size, class_num):
         super().__init__()
-        self.rnn = torch.nn.LSTM(feature_num, hidden_size=rnn_hidden_size, batch_first=True)
+        self.rnn = torch.nn.LSTM(
+            feature_num, hidden_size=rnn_hidden_size, batch_first=True
+        )
         self.fcn = torch.nn.Linear(rnn_hidden_size, class_num)
 
     def forward(self, data):
@@ -104,62 +141,93 @@ def train(model, train_dataloader, val_dataloader, optimizer):
                 label_collector += y.cpu().tolist()
         probability_collector = np.asarray(probability_collector)
         label_collector = np.asarray(label_collector)
-        classification_metrics = cal_classification_metrics(probability_collector, label_collector)
-        if best_ROCAUC < classification_metrics['ROC_AUC']:
+        classification_metrics = cal_classification_metrics(
+            probability_collector, label_collector
+        )
+        if best_ROCAUC < classification_metrics["ROC_AUC"]:
             current_patience = patience
-            best_ROCAUC = classification_metrics['ROC_AUC']
+            best_ROCAUC = classification_metrics["ROC_AUC"]
             # save model
-            saving_path = os.path.join(args.sub_model_saving,
-                                       'model_epoch_{}_ROCAUC_{:.4f}'.format(epoch, best_ROCAUC))
+            saving_path = os.path.join(
+                args.sub_model_saving,
+                "model_epoch_{}_ROCAUC_{:.4f}".format(epoch, best_ROCAUC),
+            )
             torch.save(model.state_dict(), saving_path)
         else:
             current_patience -= 1
         if current_patience == 0:
             break
-    logger.info('All done. Training finished.')
+    logger.info("All done. Training finished.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root_dir', type=str, help='model and log saving dir')
-    parser.add_argument('--original_dataset_path', type=str, help='path of original dataset')
-    parser.add_argument('--imputed_dataset_path', type=str, help='path of imputed dataset')
-    parser.add_argument('--seq_len', type=int, help='sequence length')
-    parser.add_argument('--feature_num', type=int, help='feature num')
-    parser.add_argument('--rnn_hidden_size', type=int, help='RNN hidden size')
-    parser.add_argument('--epochs', type=int, default=100, help='max training epochs')
-    parser.add_argument('--lr', type=float, help='learning rate')
-    parser.add_argument('--test_mode', dest='test_mode', action='store_true', help='test mode to test saved model')
-    parser.add_argument('--saved_model_path', type=str, default=None, help='test mode to test saved model')
-    parser.add_argument('--device', type=str, default='cuda', help='device to run model')
+    parser.add_argument("--root_dir", type=str, help="model and log saving dir")
+    parser.add_argument(
+        "--original_dataset_path", type=str, help="path of original dataset"
+    )
+    parser.add_argument(
+        "--imputed_dataset_path", type=str, help="path of imputed dataset"
+    )
+    parser.add_argument("--seq_len", type=int, help="sequence length")
+    parser.add_argument("--feature_num", type=int, help="feature num")
+    parser.add_argument("--rnn_hidden_size", type=int, help="RNN hidden size")
+    parser.add_argument("--epochs", type=int, default=100, help="max training epochs")
+    parser.add_argument("--lr", type=float, help="learning rate")
+    parser.add_argument(
+        "--test_mode",
+        dest="test_mode",
+        action="store_true",
+        help="test mode to test saved model",
+    )
+    parser.add_argument(
+        "--saved_model_path",
+        type=str,
+        default=None,
+        help="test mode to test saved model",
+    )
+    parser.add_argument(
+        "--device", type=str, default="cuda", help="device to run model"
+    )
 
     args = parser.parse_args()
     if args.test_mode:
-        assert args.saved_model_path is not None, 'saved_model_path must be provided in test mode'
+        assert (
+            args.saved_model_path is not None
+        ), "saved_model_path must be provided in test mode"
 
     # create dirs
-    time_now = datetime.now().__format__('%Y-%m-%d_T%H:%M:%S')
-    log_saving = os.path.join(args.root_dir, 'logs')
-    model_saving = os.path.join(args.root_dir, 'models')
+    time_now = datetime.now().__format__("%Y-%m-%d_T%H:%M:%S")
+    log_saving = os.path.join(args.root_dir, "logs")
+    model_saving = os.path.join(args.root_dir, "models")
     args.sub_model_saving = os.path.join(model_saving, time_now)
-    [os.makedirs(dir_) for dir_ in [model_saving, log_saving, args.sub_model_saving] if not os.path.exists(dir_)]
+    [
+        os.makedirs(dir_)
+        for dir_ in [model_saving, log_saving, args.sub_model_saving]
+        if not os.path.exists(dir_)
+    ]
     # create logger
-    logger = setup_logger(os.path.join(log_saving, 'log_' + time_now), 'w')
-    logger.info(f'args: {args}')
+    logger = setup_logger(os.path.join(log_saving, "log_" + time_now), "w")
+    logger.info(f"args: {args}")
     # build models and dataloaders
     model = SimpleRNNClassification(args.feature_num, args.rnn_hidden_size, 1)
-    dataloader = ImputedDataLoader(args.original_dataset_path, args.imputed_dataset_path,
-                                   args.seq_len, args.feature_num, 128)
+    dataloader = ImputedDataLoader(
+        args.original_dataset_path,
+        args.imputed_dataset_path,
+        args.seq_len,
+        args.feature_num,
+        128,
+    )
     train_set_loader, val_set_loader, test_set_loader = dataloader.get_loaders()
-    if 'cuda' in args.device and torch.cuda.is_available():
+    if "cuda" in args.device and torch.cuda.is_available():
         model = model.to(args.device)
 
     if not args.test_mode:
-        logger.info('Start training...')
+        logger.info("Start training...")
         optimizer = torch.optim.Adam(model.parameters(), args.lr)
         train(model, train_set_loader, val_set_loader, optimizer)
     else:
-        logger.info('Start testing...')
+        logger.info("Start testing...")
         checkpoint = torch.load(args.saved_model_path)
         model.load_state_dict(checkpoint)
         model.eval()
@@ -171,6 +239,8 @@ if __name__ == '__main__':
             label_collector += y.cpu().tolist()
         probability_collector = np.asarray(probability_collector)
         label_collector = np.asarray(label_collector)
-        classification_metrics = cal_classification_metrics(probability_collector, label_collector)
+        classification_metrics = cal_classification_metrics(
+            probability_collector, label_collector
+        )
         for k, v in classification_metrics.items():
-            logger.info(f'{k}: {v}')
+            logger.info(f"{k}: {v}")
--- dataset_generating_scripts/gene_UCI_BeijingAirQuality_dataset.py
+++ dataset_generating_scripts/gene_UCI_BeijingAirQuality_dataset.py
@@ -1,3 +1,30 @@
+"""
+The script for generating UCI Beijing Air Quality dataset.
+
+If you use code in this repository, please cite our paper as below. Many thanks.
+
+@article{DU2023SAITS,
+title = {{SAITS: Self-Attention-based Imputation for Time Series}},
+journal = {Expert Systems with Applications},
+volume = {219},
+pages = {119619},
+year = {2023},
+issn = {0957-4174},
+doi = {https://doi.org/10.1016/j.eswa.2023.119619},
+url = {https://www.sciencedirect.com/science/article/pii/S0957417423001203},
+author = {Wenjie Du and David Cote and Yan Liu},
+}
+
+or
+
+Wenjie Du, David Cote, and Yan Liu. SAITS: Self-Attention-based Imputation for Time Series. Expert Systems with Applications, 219:119619, 2023. https://doi.org/10.1016/j.eswa.2023.119619
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: GLP-v3
+
+
 import argparse
 import os
 import sys
@@ -5,27 +32,44 @@ import sys
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-sys.path.append('..')
+sys.path.append("..")
 from modeling.utils import setup_logger
-from dataset_generating_scripts.data_processing_utils import window_truncate, add_artificial_mask, saving_into_h5
+from dataset_generating_scripts.data_processing_utils import (
+    window_truncate,
+    add_artificial_mask,
+    saving_into_h5,
+)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate UCI air quality dataset')
-    parser.add_argument("--file_path", help='path of dataset file', type=str)
-    parser.add_argument("--artificial_missing_rate", help='artificially mask out additional values',
-                        type=float, default=0.1)
-    parser.add_argument("--seq_len", help='sequence length', type=int, default=100)
-    parser.add_argument('--dataset_name', help='name of generated dataset, will be the name of saving dir', type=str,
-                        default='test')
-    parser.add_argument('--saving_path', type=str, help='parent dir of generated dataset', default='.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate UCI air quality dataset")
+    parser.add_argument("--file_path", help="path of dataset file", type=str)
+    parser.add_argument(
+        "--artificial_missing_rate",
+        help="artificially mask out additional values",
+        type=float,
+        default=0.1,
+    )
+    parser.add_argument("--seq_len", help="sequence length", type=int, default=100)
+    parser.add_argument(
+        "--dataset_name",
+        help="name of generated dataset, will be the name of saving dir",
+        type=str,
+        default="test",
+    )
+    parser.add_argument(
+        "--saving_path", type=str, help="parent dir of generated dataset", default="."
+    )
     args = parser.parse_args()
 
     dataset_saving_dir = os.path.join(args.saving_path, args.dataset_name)
     if not os.path.exists(dataset_saving_dir):
         os.makedirs(dataset_saving_dir)
 
-    logger = setup_logger(os.path.join(dataset_saving_dir + "/dataset_generating.log"),
-                          'Generate UCI air quality dataset', mode='w')
+    logger = setup_logger(
+        os.path.join(dataset_saving_dir + "/dataset_generating.log"),
+        "Generate UCI air quality dataset",
+        mode="w",
+    )
     logger.info(args)
 
     df_collector = []
@@ -34,36 +78,46 @@ if __name__ == '__main__':
     for filename in file_list:
         file_path = os.path.join(args.file_path, filename)
         current_df = pd.read_csv(file_path)
-        current_df['date_time'] = pd.to_datetime(current_df[['year', 'month', 'day', 'hour']])
-        station_name_collector.append(current_df.loc[0, 'station'])
+        current_df["date_time"] = pd.to_datetime(
+            current_df[["year", "month", "day", "hour"]]
+        )
+        station_name_collector.append(current_df.loc[0, "station"])
         # remove duplicated date info and wind direction, which is a categorical col
-        current_df = current_df.drop(['year', 'month', 'day', 'hour', 'wd', 'No', 'station'], axis=1)
+        current_df = current_df.drop(
+            ["year", "month", "day", "hour", "wd", "No", "station"], axis=1
+        )
         df_collector.append(current_df)
-        logger.info(f'reading {file_path}, data shape {current_df.shape}')
+        logger.info(f"reading {file_path}, data shape {current_df.shape}")
 
-    logger.info(f'There are total {len(station_name_collector)} stations, they are {station_name_collector}')
-    date_time = df_collector[0]['date_time']
-    df_collector = [i.drop('date_time', axis=1) for i in df_collector]
+    logger.info(
+        f"There are total {len(station_name_collector)} stations, they are {station_name_collector}"
+    )
+    date_time = df_collector[0]["date_time"]
+    df_collector = [i.drop("date_time", axis=1) for i in df_collector]
     df = pd.concat(df_collector, axis=1)
-    args.feature_names = [station + '_' + feature
-                          for station in station_name_collector
-                          for feature in df_collector[0].columns]
+    args.feature_names = [
+        station + "_" + feature
+        for station in station_name_collector
+        for feature in df_collector[0].columns
+    ]
     args.feature_num = len(args.feature_names)
     df.columns = args.feature_names
-    logger.info(f'Original df missing rate: '
-                f'{(df[args.feature_names].isna().sum().sum() / (df.shape[0] * args.feature_num)):.3f}')
+    logger.info(
+        f"Original df missing rate: "
+        f"{(df[args.feature_names].isna().sum().sum() / (df.shape[0] * args.feature_num)):.3f}"
+    )
 
-    df['date_time'] = date_time
-    unique_months = df['date_time'].dt.to_period('M').unique()
+    df["date_time"] = date_time
+    unique_months = df["date_time"].dt.to_period("M").unique()
     selected_as_test = unique_months[:10]  # select first 3 months as test set
-    logger.info(f'months selected as test set are {selected_as_test}')
+    logger.info(f"months selected as test set are {selected_as_test}")
     selected_as_val = unique_months[10:20]  # select the 4th - the 6th months as val set
-    logger.info(f'months selected as val set are {selected_as_val}')
+    logger.info(f"months selected as val set are {selected_as_val}")
     selected_as_train = unique_months[20:]  # use left months as train set
-    logger.info(f'months selected as train set are {selected_as_train}')
-    test_set = df[df['date_time'].dt.to_period('M').isin(selected_as_test)]
-    val_set = df[df['date_time'].dt.to_period('M').isin(selected_as_val)]
-    train_set = df[df['date_time'].dt.to_period('M').isin(selected_as_train)]
+    logger.info(f"months selected as train set are {selected_as_train}")
+    test_set = df[df["date_time"].dt.to_period("M").isin(selected_as_test)]
+    val_set = df[df["date_time"].dt.to_period("M").isin(selected_as_val)]
+    train_set = df[df["date_time"].dt.to_period("M").isin(selected_as_train)]
 
     scaler = StandardScaler()
     train_set_X = scaler.fit_transform(train_set.loc[:, args.feature_names])
@@ -74,22 +128,32 @@ if __name__ == '__main__':
     val_set_X = window_truncate(val_set_X, args.seq_len)
     test_set_X = window_truncate(test_set_X, args.seq_len)
 
-    train_set_dict = add_artificial_mask(train_set_X, args.artificial_missing_rate, 'train')
-    val_set_dict = add_artificial_mask(val_set_X, args.artificial_missing_rate, 'val')
-    test_set_dict = add_artificial_mask(test_set_X, args.artificial_missing_rate, 'test')
-    logger.info(f'In val set, num of artificially-masked values: {val_set_dict["indicating_mask"].sum()}')
-    logger.info(f'In test set, num of artificially-masked values: {test_set_dict["indicating_mask"].sum()}')
+    train_set_dict = add_artificial_mask(
+        train_set_X, args.artificial_missing_rate, "train"
+    )
+    val_set_dict = add_artificial_mask(val_set_X, args.artificial_missing_rate, "val")
+    test_set_dict = add_artificial_mask(
+        test_set_X, args.artificial_missing_rate, "test"
+    )
+    logger.info(
+        f'In val set, num of artificially-masked values: {val_set_dict["indicating_mask"].sum()}'
+    )
+    logger.info(
+        f'In test set, num of artificially-masked values: {test_set_dict["indicating_mask"].sum()}'
+    )
 
     processed_data = {
-        'train': train_set_dict,
-        'val': val_set_dict,
-        'test': test_set_dict
+        "train": train_set_dict,
+        "val": val_set_dict,
+        "test": test_set_dict,
     }
 
-    logger.info(f'Feature num: {args.feature_num},\n'
-                f'Sample num in train set: {len(train_set_dict["X"])}\n'
-                f'Sample num in val set: {len(val_set_dict["X"])}\n'
-                f'Sample num in test set: {len(test_set_dict["X"])}\n')
+    logger.info(
+        f"Feature num: {args.feature_num},\n"
+        f'Sample num in train set: {len(train_set_dict["X"])}\n'
+        f'Sample num in val set: {len(val_set_dict["X"])}\n'
+        f'Sample num in test set: {len(test_set_dict["X"])}\n'
+    )
 
     saving_into_h5(dataset_saving_dir, processed_data, classification_dataset=False)
-    logger.info(f'All done. Saved to {dataset_saving_dir}.')
+    logger.info(f"All done. Saved to {dataset_saving_dir}.")
