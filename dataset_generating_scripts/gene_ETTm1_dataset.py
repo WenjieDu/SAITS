2c2e0545da3d08ecaec18f6841de115c8c5c8e13
Wenjie Du <wenjay.du@gmail.com>
2023-07-06 00:04:17 +0800

    Updating scripts to enable dataset ETT generating (#25)
    * feat: update scripts to enable dataset ETT generating;

--- dataset_generating_scripts/gene_ETTm1_dataset.py
+++ dataset_generating_scripts/gene_ETTm1_dataset.py
@@ -1,5 +1,5 @@
 """
-The script for generating UCI Beijing Air Quality dataset.
+The script for generating ETT-m1 dataset.
 
 If you use code in this repository, please cite our paper as below. Many thanks.
 
@@ -21,6 +21,7 @@ Wenjie Du, David Cote, and Yan Liu. SAITS: Self-Attention-based Imputation for T
 
 """
 
+
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: MIT
 
@@ -29,6 +30,7 @@ import argparse
 import os
 import sys
 
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from tsdb import pickle_dump
@@ -37,12 +39,13 @@ sys.path.append("..")
 from modeling.utils import setup_logger
 from dataset_generating_scripts.data_processing_utils import (
     window_truncate,
+    random_mask,
     add_artificial_mask,
     saving_into_h5,
 )
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate UCI air quality dataset")
+    parser = argparse.ArgumentParser(description="Generate ETTm1 dataset")
     parser.add_argument("--file_path", help="path of dataset file", type=str)
     parser.add_argument(
         "--artificial_missing_rate",
@@ -50,7 +53,8 @@ if __name__ == "__main__":
         type=float,
         default=0.1,
     )
-    parser.add_argument("--seq_len", help="sequence length", type=int, default=100)
+    parser.add_argument("--seq_len", help="sequence length", type=int, default=24)
+    parser.add_argument("--sliding_len", help="sequence length", type=int, default=12)
     parser.add_argument(
         "--dataset_name",
         help="name of generated dataset, will be the name of saving dir",
@@ -68,66 +72,47 @@ if __name__ == "__main__":
 
     logger = setup_logger(
         os.path.join(dataset_saving_dir + "/dataset_generating.log"),
-        "Generate UCI air quality dataset",
+        "Generate ETTm1 dataset",
         mode="w",
     )
     logger.info(args)
 
-    df_collector = []
-    station_name_collector = []
-    file_list = os.listdir(args.file_path)
-    for filename in file_list:
-        file_path = os.path.join(args.file_path, filename)
-        current_df = pd.read_csv(file_path)
-        current_df["date_time"] = pd.to_datetime(
-            current_df[["year", "month", "day", "hour"]]
-        )
-        station_name_collector.append(current_df.loc[0, "station"])
-        # remove duplicated date info and wind direction, which is a categorical col
-        current_df = current_df.drop(
-            ["year", "month", "day", "hour", "wd", "No", "station"], axis=1
-        )
-        df_collector.append(current_df)
-        logger.info(f"reading {file_path}, data shape {current_df.shape}")
+    df = pd.read_csv(args.file_path, index_col="date")
+    df.index = pd.to_datetime(df.index)
+    feature_names = df.columns.tolist()
+    feature_num = len(feature_names)
+    df["datetime"] = pd.to_datetime(df.index)
 
-    logger.info(
-        f"There are total {len(station_name_collector)} stations, they are {station_name_collector}"
-    )
-    date_time = df_collector[0]["date_time"]
-    df_collector = [i.drop("date_time", axis=1) for i in df_collector]
-    df = pd.concat(df_collector, axis=1)
-    args.feature_names = [
-        station + "_" + feature
-        for station in station_name_collector
-        for feature in df_collector[0].columns
-    ]
-    args.feature_num = len(args.feature_names)
-    df.columns = args.feature_names
-    logger.info(
-        f"Original df missing rate: "
-        f"{(df[args.feature_names].isna().sum().sum() / (df.shape[0] * args.feature_num)):.3f}"
-    )
-
-    df["date_time"] = date_time
-    unique_months = df["date_time"].dt.to_period("M").unique()
-    selected_as_test = unique_months[:10]  # select first 3 months as test set
+    unique_months = df["datetime"].dt.to_period("M").unique()
+    selected_as_test = unique_months[:4]  # select first 4 months as test set
     logger.info(f"months selected as test set are {selected_as_test}")
-    selected_as_val = unique_months[10:20]  # select the 4th - the 6th months as val set
+    selected_as_val = unique_months[4:8]  # select the 4th - the 8th months as val set
     logger.info(f"months selected as val set are {selected_as_val}")
-    selected_as_train = unique_months[20:]  # use left months as train set
+    selected_as_train = unique_months[8:]  # use left months as train set
     logger.info(f"months selected as train set are {selected_as_train}")
-    test_set = df[df["date_time"].dt.to_period("M").isin(selected_as_test)]
-    val_set = df[df["date_time"].dt.to_period("M").isin(selected_as_val)]
-    train_set = df[df["date_time"].dt.to_period("M").isin(selected_as_train)]
+    test_set = df[df["datetime"].dt.to_period("M").isin(selected_as_test)]
+    val_set = df[df["datetime"].dt.to_period("M").isin(selected_as_val)]
+    train_set = df[df["datetime"].dt.to_period("M").isin(selected_as_train)]
 
     scaler = StandardScaler()
-    train_set_X = scaler.fit_transform(train_set.loc[:, args.feature_names])
-    val_set_X = scaler.transform(val_set.loc[:, args.feature_names])
-    test_set_X = scaler.transform(test_set.loc[:, args.feature_names])
+    train_set_X = scaler.fit_transform(train_set.loc[:, feature_names])
+    val_set_X = scaler.transform(val_set.loc[:, feature_names])
+    test_set_X = scaler.transform(test_set.loc[:, feature_names])
 
-    train_set_X = window_truncate(train_set_X, args.seq_len)
-    val_set_X = window_truncate(val_set_X, args.seq_len)
-    test_set_X = window_truncate(test_set_X, args.seq_len)
+    train_set_X = window_truncate(train_set_X, args.seq_len, args.sliding_len)
+    val_set_X = window_truncate(val_set_X, args.seq_len, args.sliding_len)
+    test_set_X = window_truncate(test_set_X, args.seq_len, args.sliding_len)
+
+    # add missing values in train set manually
+    if args.artificial_missing_rate > 0:
+        train_set_X_shape = train_set_X.shape
+        train_set_X = train_set_X.reshape(-1)
+        indices = random_mask(train_set_X, args.artificial_missing_rate)
+        train_set_X[indices] = np.nan
+        train_set_X = train_set_X.reshape(train_set_X_shape)
+        logger.info(
+            f"Already masked out {args.artificial_missing_rate * 100}% values in train set"
+        )
 
     train_set_dict = add_artificial_mask(
         train_set_X, args.artificial_missing_rate, "train"
@@ -148,14 +133,17 @@ if __name__ == "__main__":
         "val": val_set_dict,
         "test": test_set_dict,
     }
-
+    train_sample_num = len(train_set_dict["X"])
+    val_sample_num = len(val_set_dict["X"])
+    test_sample_num = len(test_set_dict["X"])
+    total_sample_num = train_sample_num + val_sample_num + test_sample_num
     logger.info(
-        f"Feature num: {args.feature_num},\n"
-        f'Sample num in train set: {len(train_set_dict["X"])}\n'
-        f'Sample num in val set: {len(val_set_dict["X"])}\n'
-        f'Sample num in test set: {len(test_set_dict["X"])}\n'
+        f"Feature num: {feature_num},\n"
+        f"{train_sample_num} ({(train_sample_num / total_sample_num):.3f}) samples in train set\n"
+        f"{val_sample_num} ({(val_sample_num / total_sample_num):.3f}) samples in val set\n"
+        f"{test_sample_num} ({(test_sample_num / total_sample_num):.3f}) samples in test set\n"
     )
 
     saving_into_h5(dataset_saving_dir, processed_data, classification_dataset=False)
-    pickle_dump(scaler, os.path.join(dataset_saving_dir, 'scaler'))
+    pickle_dump(scaler, os.path.join(dataset_saving_dir, "scaler"))
     logger.info(f"All done. Saved to {dataset_saving_dir}.")
